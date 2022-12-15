5f543581af6633096f905e30fe4cc599bec6ae21
Gautam Singla <76390562+ggsingla@users.noreply.github.com>
2022-12-15 18:28:29 +0000

    Dean Message Section done

--- js/data.js
+++ js/data.js
@@ -108,83 +108,115 @@ fetch('https://wdmc.onrender.com/latestEvent/get/all')
 
 fetch('https://wdmc.onrender.com/administration/get/all')
     .then((response) => response.json())
-    .then((data) => {
+  .then((data) => {
+// Original Element
+// //////////////////
+// <div class="flex flex-col space-y-6 rounded-lg bg-light-purple p-6 shadow-md md:flex-row md:space-y-0 md:space-x-6">
+//       <img src="${data[0].image}" class="h-32 w-32 rounded-xl shadow-lg"
+//         alt="Director's Image" />
+//       <div id="content" class="flex flex-col space-y-4">
+//         <div class="flex space-x-4 items-center justify-start group">
+//           <h1 class="text-4xl font-bold text-accent">
+//             Director's <span class="text-dark-purple">Message</span>
+//           </h1>
+//           <span class="material-symbols-outlined group-hover:animate-shake animate-delay"
+//             style="font-size: 40px; color: var(--accent)">
+//             history_edu
+//           </span>
+//         </div>
+//         <h2 class="text-xl font-medium text-dark-purple">
+//          ${data[0].name}
+//         </h2>
+//         <p>
+//         ${data[0].messageText}
+//          </p>
+//         <div class="mt-auto flex whitespace-nowrap items-center justify-start space-x-3">
+//           <a class="cursor-pointer font-medium text-sky-500 hover:text-sky-600">Read All &rarr;</a>
+//         </div>
+//       </div>
+// </div>
 
-        const directorMessage = document.getElementById("Director's Corner ")
-        data.forEach((directormsg) => {
-            const directorMessages = document.createElement('div')
-            directorMessage.setAttribute(
-                'class',
-                'container mx-auto my-10'
-            )
-            directorMessage.innerHTML = `
-            <div class="flex flex-col space-y-6 rounded-lg bg-light-purple p-6 shadow-md md:flex-row md:space-y-0 md:space-x-6">
-            <img src=${directormsg.image}
-              alt="Director's Image" />
-            <div id="content" class="flex flex-col space-y-4">
-              <div class="flex space-x-4 items-center justify-start group">
-                <h1 class="text-4xl font-bold text-accent">
-                  Director's <span class="text-dark-purple">Message</span>
-                </h1>
-                <span class="material-symbols-outlined group-hover:animate-shake animate-delay"
-                  style="font-size: 40px; color: var(--accent)">
-                  history_edu
-                </span>
-              </div>
-              <h2 class="text-xl font-medium text-dark-purple">
-                ${directormsg.name}
-              </h2>
-              <p>
-                ${directormsg.messageText}
-              </p>
-              <div class="mt-auto flex whitespace-nowrap items-center justify-start space-x-3">
-                <a class="cursor-pointer font-medium text-sky-500 hover:text-sky-600">Read All &rarr;</a>
-              </div>
-            </div>
-          </div>
-    `
-            directorMessage.appendChild(directorMessages)
-        })
-    })
-`< section id = "placement-stats"
-class = "my-6 scroll-mt-32 bg-[url('./public/assets/images/placementStats.jpg')] bg-cover bg-fixed bg-center bg-no-repeat" >
-    <div class = "bg-dark-purple/70" >
-    <div class = "container mx-auto flex flex-col space-y-12 md:px-10 px-4 py-10 md:flex-row md:items-center md:justify-between md:max-w-screen-xl md:space-y-0" >
-    <h1 class = "text-5xl font-bold uppercase text-white" > #numbers < br / >
-    <span style = " -webkit - text - stroke: 1 px# ffffff!important; 
-    -webkit - text - fill - color: transparent;
-" class="inline - block ">that prove</span>NITJ
-    </h1> <div class = "grid grid-cols-2 gap-10 text-white md:max-w-sm"id = "stats" >
-    <div class = "number" >
-    <h1 class = "text-5xl font-bold uppercase" > < span > 995 < /span>+</h1 >
-    <p class = "text-lg uppercase" > Students placed < /p> </div> 
-    <div class = "number" >
-    <h1 class = "text-5xl font-bold uppercase" > < span > 150 < /span>+</h1 >
-    <p class = "text-lg uppercase" > Companies visited < /p> </div> 
-    <div class = "number" >
-    <h1 class = "text-5xl font-bold uppercase" > < span > 1.2 < /span> CR</h1 >
-    < p class = "text-lg uppercase" > Highest package < /p> </div> 
-    <div class = "number" >
-    <h1 class = "text-5xl font-bold uppercase" > < span > 15 < /span>+</h1 >
-    <p class = "text-lg uppercase" > startups < /p> 
-    </div> 
-    </div> 
-    </div> 
-    </div> 
-    </section>`
-fetch('https://wdmc.onrender.com/ranking/get/all')
-    .then((response) => response.json())
-    .then((data) => {
-            // //////////////////////
-            // Original News Card Element
-            // //////////////////////
-            // <div class="rounded-xl p-4 shadow-md odd:bg-blue-100/50 even:bg-blue-200">
-            //   <div class="flex flex-col items-start justify-start space-y-1 border-l-4 border-gray-800 pl-5">
-            //     <p class="w-full text-lg font-semibold">This is a headline</p>
-            //     <p class="w-full line-clamp-2">
-            //       Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem
-            //       ipsum dolor sit 2
-            //     </p>
-            //   </div>
-            // </div>
-            const placementStats = document.getElementById('placement-stats')
+    console.log(data)
+    
+        const directorMessage = document.getElementById("director-message")
+        
+        const msg = document.createElement('div')
+        msg.setAttribute(
+            'class',
+            'flex flex-col space-y-6 rounded-lg bg-light-purple p-6 shadow-md md:flex-row md:space-y-0 md:space-x-6'
+        )
+
+    // msg.setAttribute('class', 'flex flex-col')
+    msg.innerHTML = `
+        <img src="${data[0].image}" class="h-32 w-32 rounded-xl shadow-lg"
+    alt="Director's Image" />
+  <div id="content" class="flex flex-col space-y-4">
+    <div class="flex space-x-4 items-center justify-start group">
+      <h1 class="text-4xl font-bold text-accent">
+        Director's <span class="text-dark-purple">Message</span>
+      </h1>
+      <span class="material-symbols-outlined group-hover:animate-shake animate-delay"
+        style="font-size: 40px; color: var(--accent)">
+        history_edu
+      </span>
+    </div>
+    <h2 class="text-xl font-medium text-dark-purple">
+      ${data[0].name}
+    </h2>
+    <p class = "line-clamp-3">
+    ${data[0].messageText}
+      </p>
+    <div class="mt-auto flex whitespace-nowrap items-center justify-start space-x-3">
+      <a class="cursor-pointer font-medium text-sky-500 hover:text-sky-600">Read All &rarr;</a>
+    </div>
+`
+            directorMessage.appendChild(msg)
+  })
+  
+
+
+
+
+
+// `< section id = "placement-stats"
+// class = "my-6 scroll-mt-32 bg-[url('./public/assets/images/placementStats.jpg')] bg-cover bg-fixed bg-center bg-no-repeat" >
+//     <div class = "bg-dark-purple/70" >
+//     <div class = "container mx-auto flex flex-col space-y-12 md:px-10 px-4 py-10 md:flex-row md:items-center md:justify-between md:max-w-screen-xl md:space-y-0" >
+//     <h1 class = "text-5xl font-bold uppercase text-white" > #numbers < br / >
+//     <span style = " -webkit - text - stroke: 1 px# ffffff!important; 
+//     -webkit - text - fill - color: transparent;
+// " class="inline - block ">that prove</span>NITJ
+//     </h1> <div class = "grid grid-cols-2 gap-10 text-white md:max-w-sm"id = "stats" >
+//     <div class = "number" >
+//     <h1 class = "text-5xl font-bold uppercase" > < span > 995 < /span>+</h1 >
+//     <p class = "text-lg uppercase" > Students placed < /p> </div> 
+//     <div class = "number" >
+//     <h1 class = "text-5xl font-bold uppercase" > < span > 150 < /span>+</h1 >
+//     <p class = "text-lg uppercase" > Companies visited < /p> </div> 
+//     <div class = "number" >
+//     <h1 class = "text-5xl font-bold uppercase" > < span > 1.2 < /span> CR</h1 >
+//     < p class = "text-lg uppercase" > Highest package < /p> </div> 
+//     <div class = "number" >
+//     <h1 class = "text-5xl font-bold uppercase" > < span > 15 < /span>+</h1 >
+//     <p class = "text-lg uppercase" > startups < /p> 
+//     </div> 
+//     </div> 
+//     </div> 
+//     </div> 
+//     </section>`
+// fetch('https://wdmc.onrender.com/ranking/get/all')
+//     .then((response) => response.json())
+//     .then((data) => {
+//             // //////////////////////
+//             // Original News Card Element
+//             // //////////////////////
+//             // <div class="rounded-xl p-4 shadow-md odd:bg-blue-100/50 even:bg-blue-200">
+//             //   <div class="flex flex-col items-start justify-start space-y-1 border-l-4 border-gray-800 pl-5">
+//             //     <p class="w-full text-lg font-semibold">This is a headline</p>
+//             //     <p class="w-full line-clamp-2">
+//             //       Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem
+//             //       ipsum dolor sit 2
+//             //     </p>
+//             //   </div>
+//             // </div>
+//             const placementStats = document.getElementById('placement-stats')
