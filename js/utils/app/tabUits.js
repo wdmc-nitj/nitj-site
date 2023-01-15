9e9f9fa4c838e4d4a42af180ac6231290f2f429e
Gautam Singla <ggsingla777@gmail.com>
2023-01-15 11:28:00 +0530

    fetching for tabs intialized

--- js/utils/app/tabUits.js
+++ js/utils/app/tabUits.js
@@ -1,11 +1,13 @@
-export default function notificationTabs(e) {
+export const BaseURL = 'https://wdmc.onrender.com'
+
+export default async function notificationTabs(e) {
   let tabcontent = document.getElementsByClassName('notice-content')
   let tablinks = document.getElementsByClassName('notif-link')
 
   for (let i = 0; i < tabcontent.length; i++) {
     tabcontent[i].classList.add('hidden')
   }
-
+  fetchTabContent(k)
   for (let i = 0; i < tablinks.length; i++) {
     tablinks[i].classList.add('hover:bg-blue-50', 'text-slate-400')
     tablinks[i].classList.remove(
@@ -17,4 +19,10 @@ export default function notificationTabs(e) {
   e.classList.remove('hover:bg-blue-50', 'text-slate-400')
   e.classList.add('bg-blue-100', 'hover:bg-blue-200', 'text-accent')
   document.getElementById(e.dataset.notif).classList.remove('hidden')
+}
+
+async function fetchTabContent(tabName) {
+  fetch(`${BaseURL}/${tabName}`)
+    .then((res) => res.json())
+    .then((data) => console.log(data))
 }
