59f173b26b9c1bc644cacec89fde69697fa54eff
Namamish <75522655+Namamish@users.noreply.github.com>
2023-03-03 01:57:04 +0530

    Backend Integration

---
+++ research/js/jobs.js
@@ -0,0 +1,92 @@
+fetch('https://teamd.onrender.com/recruitments/updates/categorised?category=faculty')
+.then((response) => response.json())
+  .then((data) => {
+const faculty=document.getElementById("faculty");
+data.forEach((update) => {
+    const list = document.createElement('li')
+  list.innerHTML = `
+  <a
+  href=${update.link}
+  class="hover:text-[#FF6600]"
+>
+${update.title}
+ </a
+>
+
+    `
+    faculty.appendChild(list);
+  })
+
+    })
+  
+
+    fetch('https://teamd.onrender.com/recruitments/updates/categorised?category=nonFaculty')
+.then((response) => response.json())
+  .then((data) => {
+const faculty=document.getElementById("nonFaculty");
+data.forEach((update) => {
+    const list = document.createElement('li')
+  list.innerHTML = `
+  <a
+  href=${update.link}
+  class="hover:text-[#FF6600]"
+>
+${update.title}
+ </a
+>
+
+    `
+    faculty.appendChild(list);
+  })
+
+    })
+  
+
+    
+
+    fetch('https://teamd.onrender.com/recruitments/updates/categorised?category=faculty-contract')
+.then((response) => response.json())
+  .then((data) => {
+const faculty=document.getElementById("faculty-contract");
+data.forEach((update) => {
+    const list = document.createElement('li')
+  list.innerHTML = `
+  <a
+  href=${update.link}
+  class="hover:text-[#FF6600]"
+>
+${update.title}
+ </a
+>
+
+    `
+    faculty.appendChild(list);
+  })
+
+    })
+  
+
+    
+    fetch('https://teamd.onrender.com/recruitments/updates/categorised?category=nonFaculty-contract')
+.then((response) => response.json())
+  .then((data) => {
+const faculty=document.getElementById("nonFaculty-contract");
+data.forEach((update) => {
+    const list = document.createElement('li')
+  list.innerHTML = `
+  <a
+  href=${update.link}
+  class="hover:text-[#FF6600]"
+>
+${update.title}
+ </a
+>
+
+    `
+    faculty.appendChild(list);
+  })
+
+    })
+  
+
+    
